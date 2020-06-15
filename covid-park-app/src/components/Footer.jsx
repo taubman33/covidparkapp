b790e01b833b8311a4250eae71ecd6a07ec71eac
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-06-15 18:21:52 -0400

    made foot font extra small

--- covid-park-app/src/components/Footer.jsx
+++ covid-park-app/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default class Footer extends Component
     render()
     {
         return (
-            <div className="fixed mt-1 bottom-0 w-full bg-gray-100">
+            <div className="fixed text-xs bottom-0 w-full bg-gray-100">
                 <div>© Copyright Jeremy Taubman & Trevor Smith-Holbourn 2020</div>
             </div>
         )
