a3f8fc650a0bb6e6db9dc9ce571388e989aa8810
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-06-15 13:37:41 -0400

    login and logout now working on fronend

--- covid-park-app/src/components/Forms/Login.jsx
+++ covid-park-app/src/components/Forms/Login.jsx
@@ -1,33 +1,26 @@
-import React from 'react';
+import React from 'react'
 
+const Login = (props) =>
+{
 
-const Login = (props) => {
- 
-// let username = props.formData.username
   return (
-     
+
     <div>
       <hr />
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        props.handleLogin();}} >
+      <form onSubmit={(e) =>
+      {
+        e.preventDefault()
+        props.handleLogin()
+      }} >
 
         <p>Username:</p>
-        <input name="username" type="text" 
-        // value={props.formData.username} onChange={props.handleChange}  // 
-        />
-
+        <input name="email" type="text" value={props.email} onChange={props.handleChange} />
         <p>Password:</p>
-
-        <input name="password" type="password" 
-        // value={props.formData.password} onChange={props.handleChange}
-         />
-        <br/>
-        <button className="bg-green-200 border-gray-400 rounded-sm p-1 m-1"><h4>Login</h4></button>
-        
+        <input name="password" type="password" value={props.password} onChange={props.handleChange} />
+        <button className="bg-green-200 border-gray-400 rounded-sm p-1 m-1">Login</button>
       </form>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
--- covid-park-app/src/components/LoginPage.jsx
+++ covid-park-app/src/components/LoginPage.jsx
@@ -1,13 +1,46 @@
 import React, { Component } from 'react'
 import Login from './Forms/Login'
+import { handleLogin, isLoggedIn } from './services/auth'
+import { Redirect } from 'react-router-dom'
 
-export default class LoginPage extends Component {
-    render() {
-        return (
-            <div>
-                <h2> Login </h2>
-                <Login />
-            </div>
-        )
+export default class LoginPage extends Component
+{
+
+    state = {
+        email: ``,
+        password: ``
+    }
+
+    handleChange = event =>
+    {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    tryLogin = async () =>
+    {
+        await handleLogin(this.state, () =>
+        {
+            this.props.history.push("/")
+        })
+    }
+
+    render()
+    {
+        if (isLoggedIn())
+        {
+            return <Redirect to="/" />
+        }
+        else
+        {
+            return (
+                <div>
+                    <h2> Login </h2>
+                    <Login handleLogin={this.tryLogin} handleChange={this.handleChange} email={this.state.email} password={this.state.password} />
+                </div>
+            )
+        }
+
     }
 }
