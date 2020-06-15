cfdbee9e6235aa28201df7b14185cae8f60edc36
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-06-15 18:21:20 -0400

    added home button to header and added some styling to the footer

--- covid-park-app/src/components/Footer.jsx
+++ covid-park-app/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default class Footer extends Component
     render()
     {
         return (
-        <div className="fixed bottom-0">
+            <div className="fixed mt-1 bottom-0 w-full bg-gray-100">
                 <div>© Copyright Jeremy Taubman & Trevor Smith-Holbourn 2020</div>
             </div>
         )
--- covid-park-app/src/components/Header.jsx
+++ covid-park-app/src/components/Header.jsx
@@ -4,7 +4,17 @@ import { isLoggedIn, logout } from './services/auth'
 
 export default function Header(props)
 {
-
+    const homeButtonIfNotAtHomeRoute = () =>
+    {
+        if (props.location.pathname !== "/")
+        {
+            return <Link className="text-lg bg-gray-200 border-gray-400 rounded-sm p-2 m-1" to="/">Home</Link>
+        }
+        else
+        {
+            return <div className="text-lg bg-gray-100 text-gray-100 rounded-sm p-2 m-1" to="/">Home</div>
+        }
+    }
 
     const loginLogoutRegisterButtons = () =>
     {
@@ -14,25 +24,26 @@ export default function Header(props)
                 <>
                     <Link to="#"
                         onClick={() => { logout(() => { props.history.push("/") }) }}
-                        className="bg-gray-200 border-gray-400 rounded-sm p-1 m-1">Log out</Link>
-                    <Link to="/user" className="bg-gray-200 border-gray-400 rounded-sm p-1 m-1">Profile</Link>
+                        className="text-lg bg-gray-200 border-gray-400 rounded-sm p-2 m-1">Log out</Link>
+                    <Link to="/user" className="text-lg bg-gray-200 border-gray-400 rounded-sm p-2 m-1">Profile</Link>
                 </>)
         }
         else
         {
             return (
                 <>
-                    <Link to="/login" className="bg-gray-200 border-gray-400 rounded-sm p-1 m-1">Login</Link>
-                    <Link to="/register" className="bg-gray-200 border-gray-400 rounded-sm p-1 m-1">Register</Link>
+                    <Link to="/login" className="text-lg bg-gray-200 border-gray-400 rounded-sm p-2 m-1">Login</Link>
+                    <Link to="/register" className="text-lg bg-gray-200 border-gray-400 rounded-sm p-2 m-1">Register</Link>
                 </>)
         }
     }
 
     return (
         <div className="flex bg-gray-100">
-            <Link to="/" className="text-4xl text-green-600 font-black">Park Safe</Link>
+            <Link to="/" className="text-4xl text-green-600 hover:text-green-500 font-black">Park Safe</Link>
+            {homeButtonIfNotAtHomeRoute()}
             {loginLogoutRegisterButtons()}
-        </div>
+        </div >
 
     )
 }
